4b16bb99197b7b51c5d82c266cc2b5f944883470
unknown <acceresik@gmail.com>
2025-05-02 09:31:08 +0700

    Menghapus search bar di beranda dan Mengganti nama Website di Login (Admin)

--- admin/login.php
+++ admin/login.php
@@ -41,7 +41,7 @@ if (isset($_POST["login"])) {
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Login - Freshly.id Admin</title>
+  <title>Login - Outfitz Admin</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -80,7 +80,7 @@ if (isset($_POST["login"])) {
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">Freshly.id Admin</span>
+                  <span class="d-none d-lg-block">Outfitz Admin</span>
                 </a>
               </div><!-- End Logo -->
 
--- contact.php
+++ contact.php
@@ -28,6 +28,7 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <!-- Main CSS File -->
     <link rel="stylesheet" href="assets/css/style.css">
+    
 </head>
 
 <body>
@@ -47,7 +48,7 @@
                     </div><!-- End .header-left -->
                     <nav class="main-nav" style="flex: 1; text-align: center;">
                         <ul class="menu sf-arrows" style="display: inline-flex; gap: 30px; list-style: none; margin: 0; padding: 0;">
-                            <li><a href="index.php" class="sf-with-ul">Beranda</a></li>
+                            <li><a href="index.php" class="sf-with-ul">Home</a></li>
                             <li class="megamenu-container active"><a href="belanja.php" class="sf-with-ul">Belanja</a></li>
                             <li><a href="contact.php" class="sf-with-ul">Hubungi Kami</a></li>
                         </ul>
