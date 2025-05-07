f6b3f714bf8874fcaf5809a6062ed2a2f352f3c3
unknown <acceresik@gmail.com>
2025-05-07 15:49:07 +0700

    mengganti id_kategori, nm_kategori, tb_kategori menjadi id_ktg, nm_ktg dan tb_ktg di t_kategori.php

--- admin/index.php
+++ admin/index.php
@@ -184,7 +184,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
                 </div>
               </div>
             </div>
-            <!-- End Welcome Card -->
+            <!-- End Welcome Card --> 
 
             <!-- Orders Card -->
             <div class="col-xxl-4 col-md-6">
